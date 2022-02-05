e112f5543a707ae671ffca581ddd209bee90fb2e
shubhamchatrawat <99065525+shubhamchatrawat@users.noreply.github.com>
2022-02-05 11:49:32 +0530

    helloshubham

---
+++ Helloworld.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ 
+
+  cout<<"Hello \n world\n";  //way to take output,  << insertion operator
+
+  int amount1;
+  cout<<"Enter amount1:";
+  cin>>amount1;
+
+  int amount2;
+  cout<<"Enter amout2:";
+  cin>> amount2;
+   
+   int sum= amount1+ amount2;
+   cout<<sum<<endl;
+
+
+    return 0;
+}
